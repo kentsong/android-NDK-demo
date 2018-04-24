7569228cf2ad6382b163ff90b189ceb10654fc61
Kent <kent@linkwish.com>
2018-04-25 00:27:46 +0800

    android studio 3.0 use NDK

--- app/src/main/jni/test.cpp
+++ app/src/main/jni/test.cpp
@@ -5,6 +5,6 @@
 extern "C"
 {
     JNIEXPORT jstring JNICALL Java_scut_carson_1ho_ndk_1demo_MainActivity_getFromJNI(JNIEnv *env, jobject obj ){
-    return env -> NewStringUTF("Hello i am from JNI!");
+    return env -> NewStringUTF("Hello i am from JNI123!");
 	}
 }
